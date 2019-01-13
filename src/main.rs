d1594a4c961e4c84ee85f4778322d47b876f893f
Jovin Cronin-Wilesmith <jovincroninwilesmith@Jovins-MacBook-Pro.local>
2019-01-13 13:34:29 -0800

    Messin Around

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Current {
+  percentage: f64,
+  level: String,
+  title: String,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Bank {
+  current: Current,
+  is_internal: bool,
+  historical: Vec<Current>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Banks {
+  nfcu: Bank,
+  citi: Bank,
+  simple: Bank,
+  wells: Bank,
+  usaa: Bank,
+  td: Bank,
+  #[serde(rename = "fifth-third")]
+  _fifthThird: Bank,
+  discover: Bank,
+  schwab: Bank,
+  site: Bank,
+  amex: Bank,
+  bofa: Bank,
+  api: Bank,
+  us: Bank,
+  capone360: Bank,
+  fidelity: Bank,
+  tartan: Bank,
+  suntrust: Bank,
+  pnc: Bank,
+  chase: Bank,
+}
+
+fn main() -> Result<(), reqwest::Error> {
+    let mut res = reqwest::get("https://status.plaid.com/institutions/uptime")?;
+    let body: Banks = res.json()?;
+    
+    println!("Body:\n{:?}", body);
+    
+    Ok(())
+}
+
+
